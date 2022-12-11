a26ce02039bfeccc57c41c86b04ef73cb0335de8
Eduardo Alessandro Santana de Melo <eduardo@pop-os.localdomain>
2022-12-11 17:11:23 -0300

    Projeto criado

---
+++ backend/src/main/java/com/asmeduardo/locai/LocaiApplication.java
@@ -0,0 +1,13 @@
+package com.asmeduardo.locai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocaiApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/asmeduardo/locai/LocaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asmeduardo.locai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
